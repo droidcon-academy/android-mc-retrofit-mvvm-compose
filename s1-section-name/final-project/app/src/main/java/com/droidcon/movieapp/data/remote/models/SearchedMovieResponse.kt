3b72c3353438551cb6c2d7968a6f03a3c2ec4206
breensmbaka <breens.mbaka@dlight.com>
2023-10-23 09:06:04 +0300

    Update starter and final projects

--- s1-section-name/final-project/app/src/main/java/com/droidcon/movieapp/data/remote/models/SearchedMovieResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.droidcon.movieapp.data.remote.models
-
-data class SearchedMovieResponse(
-    val searchType: String,
-    val expression: String,
-    val results: List<MoviesResponse.Item>,
-    val errorMessage: String
-)
